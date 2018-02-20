eb02b172e89dcaedff738bf5dde6ff735b65bf3f
rafatbd <rafatinfo4@gmail.com>
2018-02-20 14:44:31 +0100

    projecte

--- inc/header.php
+++ inc/header.php
@@ -1,10 +1,28 @@
 <header>
 	<!-- Image and text -->
-	<nav class="navbar navbar-light bg-faded">
-	  <a class="navbar-brand" href="#">
-	    <img src="../img/diente.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
-	    Clinica Dental
-	  </a>
-	</nav>
+	<div id="content" class="container-fluid">
+		<div class="mx-auto" style="width: 200px;">
+		<div class="row">
+				<div class="col-4">
+					<a href="#">
+						<img src="img/email.png" width="30" height="30" alt="email">
+						Demanar Cita
+					</a>
+				</div>
+				<div class="col-4">
+					<a href="#">
+						<img src="img/telefono.png" width="30" height="30" alt="telefono">
+						911333333
+					</a>
+				</div>
+				<div class="col-4">
+					<a href="#">
+						<img src="img/location.png" width="30" height="30" alt="gps">	
+						Localització
+					</a>
+				</div>					
+			</div>
+		</div>
+	</div>
 </header>
 <?php include("nav.php"); ?>
--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php include("inc/head.php");?>
-	<div id="content" class="container-fluid">
-		<div class="row">			
-		</div>
-	</div>
 <?php include("inc/footer.php");  ?> 
 
 
