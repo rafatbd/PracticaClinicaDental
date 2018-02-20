92b176a8409491832f5add55dfa30b39140505b9
rafatbd <rafatinfo4@gmail.com>
2018-02-20 15:01:44 +0100

    projecte

--- inc/header.php
+++ inc/header.php
@@ -2,20 +2,20 @@
 	<!-- Image and text -->
 	<div id="content" class="container-fluid">
 		<div class="mx-auto" style="width: 200px;">
-		<div class="row">
-				<div class="col-4">
+			<div class="row">
+				<div class="col-4 centrar">
 					<a href="#">
 						<img src="img/email.png" width="30" height="30" alt="email">
 						Demanar Cita
 					</a>
 				</div>
-				<div class="col-4">
+				<div class="col-4 centrar">
 					<a href="#">
 						<img src="img/telefono.png" width="30" height="30" alt="telefono">
 						911333333
 					</a>
 				</div>
-				<div class="col-4">
+				<div class="col-4 centrar">
 					<a href="#">
 						<img src="img/location.png" width="30" height="30" alt="gps">	
 						Localització
