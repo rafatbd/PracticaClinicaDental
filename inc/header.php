69413697d7135430a2c04305f4830b427da6e676
rafatbd <rafatinfo4@gmail.com>
2018-02-20 14:04:40 +0100

    Inici projecte

--- inc/header.php
+++ inc/header.php
@@ -1,2 +1,10 @@
-<header></header>
+<header>
+	<!-- Image and text -->
+	<nav class="navbar navbar-light bg-faded">
+	  <a class="navbar-brand" href="#">
+	    <img src="../img/diente.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+	    Clinica Dental
+	  </a>
+	</nav>
+</header>
 <?php include("nav.php"); ?>
